ec16d613a2ee0812cb1e227a6329fe5424691c3c
saderyan98 <131683878+saderyan98@users.noreply.github.com>
2023-05-30 14:26:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
-var audioSrc = 'openhat.wav';
+const audioFiles = {
+  'a': 'openhat.wav',
+  'b': 'kick.wav',
+  'c': 'snare.wav',
+  'd': 'hihat.wav',
+  'e': 'clap.wav',
+  'f': 'ride.wav',
+  'g': 'tink.wav',
+  'h': 'tom.wav',
+  'i': 'openhat.wav',
+  'j': 'kick.wav',
+  'k': 'snare.wav',
+  'l': 'hihat.wav',
+  'm': 'clap.wav',
+  'n': 'ride.wav',
+  'o': 'tink.wav',
+  'p': 'tom.wav',
+  'q': 'openhat.wav',
+  'r': 'kick.wav',
+  's': 'snare.wav',
+  't': 'hihat.wav',
+  'u': 'clap.wav',
+  'v': 'ride.wav',
+  'w': 'tink.wav',
+  'x': 'tom.wav',
+  'y': 'openhat.wav',
+  'z': 'kick.wav',
+};
+
 const button = document.getElementById("button");
 
-function playSound() {
-  var audio = new Audio(audioSrc);
+function playSound(audioFile) {
+  const audio = new Audio(audioFile);
+  audio.load();
   audio.play();
 }
 
+function handleKeyDown(event) {
+  const key = event.key.toLowerCase();
+  const audioFile = audioFiles[key];
 
-button.addEventListener('keydown', function(event) {
-  playSound();
-});
-
+  if (audioFile) {
+    playSound(audioFile);
+  }
+}
 
 button.addEventListener('click', function(event) {
-  playSound();
+playSound();})
+
+button.addEventListener('click', function(event) {
+  const keys = Object.keys(audioFiles);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+  const audioFile = audioFiles[randomKey];
+  playSound(audioFile);
 });
+
+document.addEventListener('keydown', handleKeyDown);
